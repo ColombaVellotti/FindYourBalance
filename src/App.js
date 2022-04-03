c856d661dfc6d4c4a0424407089515338e7241d1
ColombaVellotti <vellotti.colomba1996@gmail.com>
2022-04-03 14:09:25 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -7,22 +7,6 @@ import   './css/Home.css'
 
 import { Route, Routes} from 'react-router-dom'
 
-
-import React from 'react'
-import { useMediaQuery } from 'react-responsive'
-
-const Example = () => {
-    const isDesktopOrLaptop = useMediaQuery({
-        query: '(min-device-width: 1224px)'
-    })
-    const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' })
-    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-    const isTabletOrMobileDevice = useMediaQuery({
-        query: '(max-device-width: 1224px)'
-    })
-    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
-    const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
-
 function App() {
   return (
     <>
