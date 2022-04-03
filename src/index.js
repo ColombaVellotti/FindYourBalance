930db3dd43efec1c78a9a589f85536d3d1e51650
ColombaVellotti <vellotti.colomba1996@gmail.com>
2022-04-03 14:00:13 +0100

    ok

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router} from 'react-router-dom'
 
+
+import React from 'react'
+import { useMediaQuery } from 'react-responsive'
+
+const Example = () => {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-device-width: 1224px)'
+    })
+    const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    const isTabletOrMobileDevice = useMediaQuery({
+        query: '(max-device-width: 1224px)'
+    })
+    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+    const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
